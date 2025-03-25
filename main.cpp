b6d87edeeb02e96f4e69987d2e8df5bcbd311015
Javier A. Martinez <javier.martinez36@upr.edu>
2025-03-25 17:45:21 +0000

    alpha

--- main.cpp
+++ main.cpp
@@ -1,1 +1,60 @@
+/*
+*/
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+double volumen();
+double volumen();
+double volumen();
+
+double area();
+double area();
+double area();
+
+int main()
+{
+    // Desplegar proposito
+    cout << "Este programa calcula el volumen y el área de la supercicie de tres figuras!";
+
+    // Declarar variable
+
+    char seleccionFigura;
+    char seleccionOperacion;
+
+    // Recibir seleccion de figura
+
+    cout << "Escoja la figura: " << endl;
+    cout << "   a. Cilindro" << endl;
+    cout << "   b. Esfera" << endl;
+    cout << "   c. Prisma Triangular" << endl;
+    cout << "Selección: ";
+    cin >> seleccionFigura;
+
+    // Descicion de figura
+
+    switch (seleccionFigura)
+    {
+        case 'a':
+        {
+
+        }
+
+        case 'b':
+        {
+
+        }
+
+        case 'c':
+        {
+
+        }
+    }
+
+    return 0;
+}
+
+
 
