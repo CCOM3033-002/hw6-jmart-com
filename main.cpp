c8439353836fc0f341a0e7eca8fd1791ba9251cc
Javier A. Martinez <javier.martinez36@upr.edu>
2025-03-25 20:22:36 +0000

    alpha 0.3

--- main.cpp
+++ main.cpp
@@ -6,13 +6,13 @@
 
 using namespace std;
 
-double volumen();
-double volumen();
-double volumen();
+double volumen(double alt, double rad); // Volumen de Cilindro
+double volumen(double rad); // Volumen de Esfera
+double volumen(double lado1, double lado2, double lado3); // Volumen de Prisma Triangular
 
-double area();
-double area();
-double area();
+double area(double alt, double rad); // Area de Cilindro
+double area(double rad); // Area de Esfera
+double area(double lon, double lado1, double lado2, double lado3, double base, double alt); // Area de Prisma Triangular
 
 int main()
 {
@@ -21,8 +21,8 @@ int main()
 
     // Declarar variable
 
-    char seleccionFigura;
-    char seleccionOperacion;
+    char seleccionFig;
+    char seleccionOp;
 
     // Recibir seleccion de figura
 
@@ -31,30 +31,111 @@ int main()
     cout << "   b. Esfera" << endl;
     cout << "   c. Prisma Triangular" << endl;
     cout << "Selección: ";
-    cin >> seleccionFigura;
+    cin >> seleccionFig;
 
     // Descicion de figura
 
-    switch (seleccionFigura)
+    switch (seleccionFig)
     {
         case 'a':
         {
+            double radio;
+            double altura;
 
+            cout << "Escoja entre las siguientes opciones: " << endl;
+            cout << "   a. Volumen" << endl;
+            cout << "   b. Base" << endl;
+            cout << "Selección: ";
+            cin >> seleccionOp;
+
+            while (seleccionOp != 'a' || seleccionOp != 'b')
+            {
+                cout << "ERROR: Entre dato valido";
+
+                cout << "Escoja entre las siguientes opciones: " << endl;
+                cout << "   a. Volumen" << endl;
+                cout << "   b. Base" << endl;
+                cout << "Selección: ";
+                cin >> seleccionOp;
+            }
+
+            if (seleccionOp = 'a')
+            {
+                cout << "Entre el radio del cilindro: ";
+                cin >> radio;
+
+                cout << "Entre la altura del cilindro: ";
+                cin >> altura;
+
+                while (radio <= 0.0 || altura <= 0.0)
+                {
+                    cout << "ERROR: Entre dato valido";
+
+                    cout << "Entre el radio del cilindro: ";
+                    cin >> radio;
+
+                    cout << "Entre la altura del cilindro: ";
+                    cin >> altura;
+                }
+
+                cout << "El volumen del cilindro es: " << volumen(altura, radio);
+            }
+
+            else
+            {
+                cout << "Entre el radio del cilindro: ";
+                cin >> radio;
+
+                cout << "Entre la altura del cilindro: ";
+                cin >> altura;
+
+                while (radio <= 0.0 || altura <= 0.0)
+                {
+                    cout << "ERROR: Entre dato valido";
+
+                    cout << "Entre el radio del cilindro: ";
+                    cin >> radio;
+
+                    cout << "Entre la altura del cilindro: ";
+                    cin >> altura;
+                }
+
+                cout << "El area del cilindro es: " << area(altura, radio);
+            }
+            
         }
 
         case 'b':
         {
-
+            cout << "Escoja la figura: " << endl;
+            cout << "   a. Volumen" << endl;
+            cout << "   b. Base" << endl;
+            cout << "Selección: ";
+            cin >> seleccionOp;
         }
 
         case 'c':
         {
-
+            cout << "Escoja la figura: " << endl;
+            cout << "   a. Volumen" << endl;
+            cout << "   b. Base" << endl;
+            cout << "Selección: ";
+            cin >> seleccionOp;
         }
     }
 
     return 0;
 }
 
+
+double volumen(double alt, double rad)
+{
+
+}
+
+double area(double alt, double rad)
+{
+    
+}
 
 
